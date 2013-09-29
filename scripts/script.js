bc08817176c72ca79175a1c971304cc1b2a92757
RU-021 <RU-021@Admins-Mac-mini-4.local>
2013-09-28 20:53:28 -0600

    done

--- scripts/script.js
+++ scripts/script.js
@@ -38,6 +38,30 @@ var enabledIntervals = [
 	[true,11],
 	[true,12]
 ];
+var keyEvents = [m2Event = $.Event('keydown'),
+    M2Event = $.Event('keydown'),
+    m3Event = $.Event('keydown'),
+    M3Event = $.Event('keydown'),
+    P4Event = $.Event('keydown'),
+    P5Event = $.Event('keydown'),
+    m6Event = $.Event('keydown'),
+    M6Event = $.Event('keydown'),
+    m7Event = $.Event('keydown'),
+    M7Event = $.Event('keydown'),
+    OEvent = $.Event('keydown')]
+
+keyEvents[0].which = 81
+keyEvents[1].which = 50
+keyEvents[2].which = 87
+keyEvents[3].which = 51
+keyEvents[4].which = 52
+keyEvents[5].which = 53
+keyEvents[6].which = 84
+keyEvents[7].which = 54
+keyEvents[8].which = 89
+keyEvents[9].which = 55
+keyEvents[10].which = 56
+
 
 var size2KeyCode = function(size){
 	if (size === 1){
@@ -76,6 +100,18 @@ var size2KeyCode = function(size){
 
 }
 
+var startTime;
+var score;
+
+var helpText = 'This app is for aspiring musicians to train their ears to recognize' +
+	' musical intervals.  Use the buttons on the right to choose which intervals you' +
+    ' want to be tested on.  The more intervals you enable, the more points you earn' +
+    " for each correct answer.  When you're ready to start, press the 'GO' button." +
+    ' You will have 30 seconds to identify as many intervals as you can.' +
+    ' You can identify intervals with the buttons on the right, or you can use your keyboard' +
+    " The keyboard controls are:  (m2nd:'Q') (M2nd:'2') (m3rd:'W') (M3rd:'3')" +
+    " (P4th:'4') (P5th:'5') (m6th:'T') (M6th:'6') (m7th:'Y') (M7th:'7') (Oct:'8')"
+
 var definePossibilities = function(intervalsArray){
 	possibilities = []
     for (var i = 0; i < intervalsArray.length; i++){
@@ -104,45 +140,123 @@ var playNotes = function(notes){
 }
 
 
-var startTime;
+
+
 
 $(document).ready(function(){
-    var trialLoop = function(){
-    	
-    	setInterval(function(){
-    		$(".timer").text(new Date() - startTime)
-    	},250)
-		trialInfo = playNotes(pickNotes(definePossibilities(enabledIntervals)))
-		$(document).one('keydown',function(event){
-			if (event.which === size2KeyCode(trialInfo["intervalSize"])){
-				console.log("correct!")
-			}
-			else {
-				console.log("nope!")
-			}
-			clearInterval(trialInfo["intervalID"])
-			if (new Date() - startTime < 30000){
-				trialLoop()
-			}
 
+    var feedback = $('.feedback')
+    var go = $('.go')
+
+    var setScoreMultiplier = function(){
+    	var multiplier = 5
+    	for (var i = 0; i < enabledIntervals.length; i++){
+    		if (enabledIntervals[i][0] === true){
+    			multiplier += 1
+
+    		}
+    	}
+    	if (multiplier < 7){
+    		multiplier = 0
+    	}
+    	return multiplier
+    }
+
+    var setGo = function(){
+    	if (go.hasClass("on")){
+    		go.removeClass("on")
+    	}
+    	go.one("click",function(){
+    		go.addClass("on")
+    		trialLoop()
+        })
+    }
+
+    var rightAnswer = function(){
+		feedback.toggleClass('right')
+		feedback.text('Yup!')
+		console.log("correct!")
+		score += 1 * scoreMultiplier
+		$('.score').text(score + ' Points')
+		setTimeout(function(){feedback.toggleClass('right')},700)
+    }
+
+    var wrongAnswer = function(){
+    	console.log("nope!")
+		feedback.toggleClass('wrong')
+		feedback.text('Nope!')
+
+		setTimeout(function(){feedback.toggleClass('wrong')},700)
+    }
+
+    var trial = function(){
+		trialInfo = playNotes(pickNotes(definePossibilities(enabledIntervals)))
+		setTimeout(function(){
+			$(document).one('keydown',function(event){
+				if (event.which === size2KeyCode(trialInfo["intervalSize"])){
+					rightAnswer()
+				}
+				else {
+					wrongAnswer()
+				}
+				clearInterval(trialInfo["intervalID"])
+				if (new Date() - startTime < 30000){
+					setTimeout(function(){trial()},600)
+				}	
+				else {
+					setGo()
+					$('.timer').toggleClass('open')
+					
+					$("#intervalEnabler .button").off("click")
+					setEnablerButtons()
+				}
+			})
+        },700)
+	}
+	
+/** Sets up the trial loop, and starts the first trial */
+	var trialLoop = function(){
+		$('.score').removeClass('open')
+		$('.feedback').removeClass('helpText')
+		score = 0
+		scoreMultiplier = setScoreMultiplier()
+		$("#intervalEnabler .button").off("click")
+		setPickerButtons()
+		setTimeout(function(){
+			startTime = new Date();
+			$('.score').text('0 Points')
+			$('.timer').toggleClass('open')
+			$('.score').toggleClass('open')
+			setInterval(function(){
+    			$(".timer").text(30 - Math.floor((new Date() - startTime)/1000) + ' Seconds')
+    		},250)
+			trial()
+		},300)
+	}
+
+	var setPickerButtons = function(){
+		$("#intervalEnabler .button").on("click", function(){
+			console.log($(this).index())
+			console.log(keyEvents[$(this).index()])
+			$('body').trigger(keyEvents[$(this).index()])
 		})
 	}
 
-	$(".button").on("click",function(){
-		$(this).toggleClass("on")
-		console.log($(this).index())
-		enabledIntervals[$(this).index()][0] = !enabledIntervals[$(this).index()][0]
-	})
-    
-    $(".go").on("click",function(){
-    	startTime = new Date();
-    	trialLoop()
+	var setEnablerButtons = function(){
+		$("#intervalEnabler .button").on("click",function(){
+			$(this).toggleClass("on")
+			console.log($(this).index())
+			enabledIntervals[$(this).index()][0] = !enabledIntervals[$(this).index()][0]
+		})
+    }
+    $('.help').on('click',function(){
+    	if (!$('.go').hasClass('on')){
+    	    $('.feedback').toggleClass('helpText')
+    	    $('.feedback').text(helpText)
+    	}
     })
-
-
-
-
-	
+    setEnablerButtons()
+    setGo()
 	
 })
 
